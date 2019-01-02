8161a1d666bcc1216716babec7d614bf878c14e8
EnriqueNestozo <enrique.flores093@gmail.com>
2019-01-01 23:25:46 -0600

    Se agregan vistas y plantillas de detalle de post y css

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.post_list, name = 'post_list'),
+	#int:pk se refiere a que a la vista le va a pasar un entero en una variable pk
+	path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
 ]
