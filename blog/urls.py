3b3c34cdfb57d058c1aba1510e35fdd26a3a3211
EnriqueNestozo <enrique.flores093@gmail.com>
2019-01-03 23:51:10 -0600

    Se agrega el boton de eliminar post y se separa la creación de la publicación de un post

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
 	#int:pk se refiere a que a la vista le va a pasar un entero en una variable pk
 	path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
 	path('post/new/', views.post_new, name = 'post_new'),
-	path('post/<int:pk>/edit', views.post_edit, name = 'post_edit'),
+	path('post/<int:pk>/edit/', views.post_edit, name = 'post_edit'),
+	path('drafts/', views.post_draft_list, name = 'post_draft_list'),
+	path('post/<int:pk>/publish/', views.post_publish, name = 'post_publish'),
+	path('post/<int:pk>/remove/', views.post_remove, name = 'post_remove'),
 ]
